cbb1e60ed6b99126722fd62d10f811c37bf735ac
workingjubilee <workingjubilee@gmail.com>
2019-02-06 10:13:42 -0800

    Some SCSS cleanup, beginning animated component.

---
+++ assets/js/compass.js
@@ -0,0 +1,29 @@
+// Concept: Compass component. Piece is a JS menu with animations.
+// Component is intended to exhibit "progressive enhancement."
+// This is roughly what the HTML and CSS should look like to begin with:
+
+// HTML reference:
+// <nav id="compassbar">
+//   <a href="#" id="compasslink"><i id="compassicon" class="major icon fa-compass"></i></a>
+// </nav>
+// CSS reference:
+// #compassbar {
+//   padding: 20px;
+//   text-align: right;
+//   position: fixed;
+//   top: 0;
+//   width: 100%;
+//   max-width: 100%;
+//   #compassicon {
+//     // display: flex;
+//     color: _palette(accent1);
+//   }
+// }
+
+class Compass {
+  constructor() {
+
+  }
+}
+
+let compasslink = document.getElementById('compasslink');
