2466513c090e106881f51cb0162946634b20a53c
workingjubilee <workingjubilee@gmail.com>
2019-02-06 15:24:53 -0800

    Compass piece built, needs menu to pop out.

--- assets/js/compass.js
+++ assets/js/compass.js
@@ -1,10 +1,12 @@
 // Concept: Compass component. Piece is a JS menu with animations.
 // Component is intended to exhibit "progressive enhancement."
+// "What does that mean?" If the person is blocking or can't run the JS file,
+// or the JS file doesn't load, it doesn't engage the meaningful menu elements.
 // This is roughly what the HTML and CSS should look like to begin with:
 
 // HTML reference:
-// <nav id="compassbar">
-//   <a href="#" id="compasslink"><i id="compassicon" class="major icon fa-compass"></i></a>
+// <nav id="compass-nav">
+//   <a href="#" id="compass-link"><i id="compass-icon" class="major icon fa-compass"></i></a>
 // </nav>
 // CSS reference:
 // #compassbar {
@@ -21,9 +23,44 @@
 // }
 
 class Compass {
-  constructor() {
-
+  constructor(parent) {
+    this.bar = parent;
+    // this.icon = this.bar.querySelector('#compass-icon');
+    this.initLink = this.bar.querySelector('#compass-link');
+    this.initLink.addEventListener('click', ()=> {this.openMenu()});
+    // icon.addEventListener('mouseover', ()=> {});
   }
+  openMenu() {
+    event.preventDefault();
+    event.stopPropagation();
+  };
 }
 
-let compasslink = document.getElementById('compasslink');
+const compassBar = document.getElementById('compass-bar');
+// const compassBar2 = document.querySelector('#compass-bar');
+console.log(compassBar);
+// console.log(compassBar2);
+new Compass(compassBar);
+
+
+// document.querySelector('.nav').addEventListener('click', () =>{
+//   event.preventDefault();
+//   alert(`Stay a while and listen!`)
+//   event.stopPropagation();
+//   // Successfully killed propagation!
+//   // Also yes, multiple alerts CAN chain! So annoying!
+// });
+
+
+// logoTag = document.querySelector('.logo-heading')
+// logoTag.addEventListener('mouseover', () => {
+//   logoTag.style.animationPlayState = "running";
+//   event.stopPropagation();
+//   // Successfully killed propagation!
+// });
+//
+// logoTag.addEventListener('mouseout', () => {
+//   logoTag.style.animationPlayState = "paused";
+//   event.stopPropagation();
+//   // together, "mouseover" and "mouseout" can reimplement a CSS :hover.
+// });
